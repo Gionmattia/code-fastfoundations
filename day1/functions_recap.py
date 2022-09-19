bba2f04af6942bcd084ad655c0b01d106bae89de
gionmattia <gionmattia@gmail.com>
2022-09-19 11:42:41 +0100

    Added notes on his code. QUite smart

--- day1/functions_recap.py
+++ day1/functions_recap.py
@@ -7,6 +7,20 @@ def calculate_geometric_series(a, r, n=10):
         return a * (n + 1)
     return a * (1 - r ** (n + 1)) / (1 - r)
 
+def get_max(a_list):
+# In my code, I was evaluating all the values in the list, so I had to consider cases such as
+# len(list == 0 and len(list)== 1
+# In his case, he is just processing each value inside the list, so he skips all of that.
+# He uses booleans to determine if he saw the first element or not.
+    maximum = None
+    first = False
+    for value in a_list:
+        if not first: # This loop works only for the first time.
+            maximum = value
+            first = True
+        if value > maximum:
+            maximum = value
+    return maximum
 
 def main():
     a = int(input("a: "))
