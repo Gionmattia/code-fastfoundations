46f07549e96381c2e8c6d3bacc8cbadb04e06309
gionmattia <gionmattia@gmail.com>
2022-09-19 15:16:29 +0100

    Updating my work for day1

--- day1/scrap_book.py
+++ day1/scrap_book.py
@@ -1,18 +1,18 @@
-
-
 string = "violet_blue|convert|red|6.3327|9.4423|113.3428|7.3298|5.3353|9.9283|over|all"
-my_list = string.split("|")
-coordinates = []
-for each in my_list:
-    if each[0].isnumeric():
-        coordinates.append(each)
-print(coordinates)
+def from_string_to_list(inpts):
+    my_list = inpts.split("|")
+    coordinates = []
+    for each in my_list:
+        if each[0].isnumeric():
+            coordinates.append(each)
+    return coordinates
 
-tup = []
-for each in range(0, int(len(coordinates)/2)):
-    t = (float(coordinates[each]),float(coordinates[each+3]))
-    tup.append(t)
-print(tup)
+def tuplizer(coordinates):
+    tup = []
+    for each in range(0, int(len(coordinates)/2)):
+        t = (float(coordinates[each]),float(coordinates[each+3]))
+        tup.append(t)
+    return tup
 
 def dist(a,b):
     """
@@ -21,11 +21,14 @@ def dist(a,b):
     d = (a-b)**2
     return d
 
-sum = 0
-for each in tup:
-    sum = sum + dist(each[0],each[1])
-distance = sum**0.5
-print(sum)
+def get_distance(tup):
+    sum = 0
+    for each in tup:
+        sum = sum + dist(each[0],each[1])
+    distance = sum**0.5
+    return distance
+
+
+tup = tuplizer((from_string_to_list(string)))
+distance = get_distance(tup)
 print(distance)
-
-
